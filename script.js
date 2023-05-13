94144cb99b5e7b0127c6439624516aac2d1bdf3b
Surendra2611 <2611surendrayadav2712@gmail.com>
2023-05-13 20:04:08 +0530

    done.

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(student){
+    if( student.marks > 50 ){
+      console.log(student);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function(student){
+    if(student.marks > 50 ){
+      console.log(student)
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = {id:4, name:"susan", age:"20" , marks:45};
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+ let failed_Students = arr.filter((student) => student.marks < 50)
+ console.log(failed_Students);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let new_Arr = [
+    { id: 4, name: "surendra", age: "25", marks: 1000 },
+    { id: 5, name: "ritu", age: "22", marks: 71 },
+    { id: 6, name: "yogendra", age: "28", marks: 99 },
+  ];
+
+  let concatenatedArr = arr.concat(new_Arr);
+  console.log(concatenatedArr);
 }
